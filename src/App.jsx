c1c6943be16f294af520714953c81cc5b0fc9465
Hajar Hamza <hajar.hamza1@gmail.com>
2022-08-10 18:45:22 +0200

    change to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Poster from './views/Poster';
 import Camera from './views/Camera';
 import Gallery from './views/Gallery';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
           <Route path="/" element={<Poster />} />
           <Route path="/camera" element={ <Camera /> } />
           <Route path="/gallery" element={ <Gallery /> } />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
