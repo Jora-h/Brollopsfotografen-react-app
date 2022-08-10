b668c82a8867713660fee2d59f7fdd855800e7b2
Hajar Hamza <hajar.hamza1@gmail.com>
2022-08-10 19:06:39 +0200

    redirect app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 
 import React from 'react';
 import './App.css';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Poster from './views/Poster';
 import Camera from './views/Camera';
 import Gallery from './views/Gallery';
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-          <Route path="/" element={<Poster />} />
-          <Route path="/camera" element={ <Camera /> } />
-          <Route path="/gallery" element={ <Gallery /> } />
-      </Routes>
-    </HashRouter>
+    <Routes>
+        <Route path="/" element={<Poster />} />
+        <Route path="/camera" element={ <Camera /> } />
+        <Route path="/gallery" element={ <Gallery /> } />
+    </Routes>
   );
 }
 
